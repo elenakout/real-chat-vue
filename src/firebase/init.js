c71335798c9990ac5a5baf989f753b41329913e8
elenakout <elenakout@gmail.com>
2019-03-07 21:49:10 +0200

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBhoskJPYjyxn7jHA7RgeX5Za5cigoL3Cw",
-    authDomain: "test1-86b27.firebaseapp.com",
-    databaseURL: "https://test1-86b27.firebaseio.com",
-    projectId: "test1-86b27",
-    storageBucket: "test1-86b27.appspot.com",
-    messagingSenderId: "613441999790"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
   const firebaseApp = firebase.initializeApp(config);
 
